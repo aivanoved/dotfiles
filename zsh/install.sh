afa62c17e4295f96c08582d42128092ee25fafc2
Alexander Ivanov <alexander.valentinov.ivanov@gmail.com>
2022-12-26 21:20:41 +0200

    Reorder install script

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,9 +1,9 @@
 rm -rf ~/.zshrc
 rm -rf ~/.zshenv
-rm -rf ~/.zshrc.pre-oh-my-zsh
 rm -rf ~/.zshsecretenv
+rm -rf ~/.zshrc.pre-oh-my-zsh
 
 ln -s ~/dotfiles/zsh/.zshrc ~/.zshrc
 ln -s ~/dotfiles/zsh/.zshenv ~/.zshenv
-ln -s ~/dotfiles/zsh/.zshrc.pre-oh-my-zsh ~/.zshrc.pre-oh-my-zsh
 ln -s ~/dotfiles/zsh/.zshsecretenv ~/.zshsecretenv
+ln -s ~/dotfiles/zsh/.zshrc.pre-oh-my-zsh ~/.zshrc.pre-oh-my-zsh
