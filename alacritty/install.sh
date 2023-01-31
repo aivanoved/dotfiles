a474b0bcae56127756a4423d6f7cc87eed1ce742
Alex <alexander.valentinov.ivanov@gmail.com>
2023-01-31 21:41:41 +0200

    Use alacritty theme

--- alacritty/install.sh
+++ alacritty/install.sh
@@ -1,3 +1,10 @@
 rm -rf ~/.alacritty.sh
 
+rm -rf ~/.config/alacritty/themes
+rm -rf ~/.config/alacritty/sonokai.yml
+
 ln -s ~/dotfiles/alacritty/.alacritty.yml ~/.alacritty.yml
+
+ln -s ~/dotfiles/alacritty/sonokai.yml ~/.config/alacritty/sonokai.yml
+
+git clone https://github.com/alacritty/alacritty-theme ~/.config/alacritty/themes
