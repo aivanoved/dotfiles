ad94857e1d00071246ae87b12a651a59e92d5464
Alexander Ivanov <alexander.valentinov.ivanov@gmail.com>
2023-02-03 02:33:00 +0200

    Add new line

--- tmux/install.sh
+++ tmux/install.sh
@@ -3,4 +3,5 @@ rm -rf ~/.tmux.conf
 rm -rf ~/.config/tmuxinator/default.yml
 
 ln -s ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
+
 ln -s ~/dotfiles/tmux/tmuxinator/default.yml ~/.config/tmuxinator/default.yml
