0652eb160eb606f2cc449fbf4aae06bc9f8198a1
Alexander Ivanov <alexander.valentinov.ivanov@gmail.com>
2023-06-09 01:43:02 +0300

    Use remap keymap

--- nvim/nvim/after/plugin/remap_vim_subversive.lua
+++ nvim/nvim/after/plugin/remap_vim_subversive.lua
@@ -1,3 +1,3 @@
-vim.keymap.set('n', 's', 'SubversiveSubstitute')
-vim.keymap.set('n', 'ss', 'SubversiveSubstituteLine')
-vim.keymap.set('n', 'S', 'SubversiveSubstituteToEndOfLine')
+vim.keymap.set('n', 's', '<plug>(SubversiveSubstitute)')
+vim.keymap.set('n', 'ss', '<plug>(SubversiveSubstituteLine)')
+vim.keymap.set('n', 'S', '<plug>(SubversiveSubstituteToEndOfLine)')
--- nvim/nvim/after/plugin/remap_vim_yoink.lua
+++ nvim/nvim/after/plugin/remap_vim_yoink.lua
@@ -1,9 +1,9 @@
-vim.keymap.set('n', '<c-n>', '<plug>YoinkPostPasteSwapBack', { remap = false })
-vim.keymap.set('n', '<c-p>', '<plug>YoinkPostPasteSwapForward', { remap = false })
+vim.keymap.set('n', '<c-n>', '<plug>(YoinkPostPasteSwapBack)', { remap = false })
+vim.keymap.set('n', '<c-p>', '<plug>(YoinkPostPasteSwapForward)', { remap = false })
 
-vim.keymap.set('n', 'p', '<plug>YoinkPaste_p', { remap = false })
-vim.keymap.set('n', 'P', '<plug>YoinkPaste_P', { remap = false })
+vim.keymap.set('n', 'p', '<plug>(YoinkPaste_p)', { remap = false })
+vim.keymap.set('n', 'P', '<plug>(YoinkPaste_P)', { remap = false })
 
 -- Also replace the default gp with yoink paste so we can toggle paste in this case too
-vim.keymap.set('n', 'gp', '<plug>YoinkPaste_gp', { remap = false })
-vim.keymap.set('n', 'gP', '<plug>YoinkPaste_gP', { remap = false })
+vim.keymap.set('n', 'gp', '<plug>(YoinkPaste_gp)', { remap = false })
+vim.keymap.set('n', 'gP', '<plug>(YoinkPaste_gP)', { remap = false })
