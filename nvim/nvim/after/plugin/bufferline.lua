7b246be6e5bafef66f48770297ea47a050aa6d92
Alexander Ivanov <alexander.valentinov.ivanov@gmail.com>
2023-06-09 03:36:12 +0300

    Use lualine, bufferline

--- nvim/nvim/after/plugin/bufferline.lua
+++ nvim/nvim/after/plugin/bufferline.lua
@@ -1,6 +1,21 @@
 local bufferline = require('bufferline')
+
 bufferline.setup {
     options = {
-        numbers = "ordinal"
+        numbers = "ordinal",
+        diagnostics = "nvim_lsp",
+        diagnostics_indicator = function(count, level, diagnostics_dict, context)
+            if context.buffer:current() then
+                return ''
+            end
+
+            local icon = level:match('error') and ' '
+                or level:match('warning') and ' '
+                or level:match('info') and '󰋽 '
+                or level:match('hint') and '󰌶 '
+                or ''
+
+            return ' ' .. icon .. count
+        end
     }
 }
--- nvim/nvim/after/plugin/lualine.lua
+++ nvim/nvim/after/plugin/lualine.lua
@@ -1,5 +1,19 @@
 require('lualine').setup {
     options = {
-        theme = "catppuccin"
-    }
+        theme = "catppuccin",
+        component_separators = {},
+        section_separators = {},
+    },
+    sections = {
+        lualine_a = {
+            {
+                'mode',
+                fmt = function(str)
+                    return str:sub(1, 1)
+                end
+            }
+        },
+        lualine_y = { 'progress', 'selectioncount', 'searchcount' },
+    },
+    extension = { 'fugitive', 'nerdtree', 'quickfix' }
 }
