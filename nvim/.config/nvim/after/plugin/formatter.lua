25cdd8fc820010bbbccd84cf0ed6f9a2ec726ab3
Alexander Ivanov <alexander.valentinov.ivanov@gmail.com>
2024-01-04 20:18:33 +0200

    remove unnecessary plugins
    add autoformat save

---
+++ nvim/.config/nvim/after/plugin/formatter.lua
@@ -0,0 +1,21 @@
+local formatter = require 'formatter'
+
+formatter.setup {
+    logging = false,
+    log_level = vim.log.levels.WARN,
+    filetype = {
+        ["*"] = {
+            require("formatter.filetypes.any").remove_trailing_whitespace
+        }
+    }
+}
+
+
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+
+augroup("__formatter__", { clear = true })
+autocmd("BufWritePost", {
+    group = "__formatter__",
+    command = ":FormatWriteLock",
+})
--- nvim/.config/nvim/after/plugin/remap_vim_subversive.lua
+++
@@ -1,3 +0,0 @@
-vim.keymap.set('n', 's', '<plug>(SubversiveSubstitute)')
-vim.keymap.set('n', 'ss', '<plug>(SubversiveSubstituteLine)')
-vim.keymap.set('n', 'S', '<plug>(SubversiveSubstituteToEndOfLine)')
