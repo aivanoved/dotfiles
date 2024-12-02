a7bf4f3d78b1226dcc3808bd47b9de2a1eb78db0
Alexander Ivanov <alexander.valentinov.ivanov@gmail.com>
2024-12-02 13:40:54 +0100

    remove unused flag

--- zsh/.config/zsh/alias_eza.zsh
+++ zsh/.config/zsh/alias_eza.zsh
@@ -1,10 +1,10 @@
-alias ea="eza -lhF"
+alias ea="eza -lh"
 alias edot="eza -ld .*"
 alias el="eza -l"
 
-alias er="eza -tRhF"
+alias er="eza -tRh"
 alias esa="eza -lah"
 alias esn="eza -1"
-alias esr="eza -lRhF"
+alias esr="eza -lRh"
 
 alias et="eza -lht modified"
