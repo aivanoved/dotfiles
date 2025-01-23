5a145b08a543a9edccda56826ef990630fdb2035
Alexander Ivanov <alexander.valentinov.ivanov@gmail.com>
2025-01-23 11:07:15 +0200

    oragnization for zsh, no more 1password

--- zsh/.config/zsh/alias.zsh
+++ zsh/.config/zsh/alias.zsh
@@ -2,12 +2,12 @@ source "$ZSH_CONFIG_DIR/util.zsh"
 
 alias :q='exit'
 
-_safe_source "$ZSH_CONFIG_DIR/alias_editor.zsh"
-_safe_source "$ZSH_CONFIG_DIR/alias_eza.zsh"
-_safe_source "$ZSH_CONFIG_DIR/alias_cd.zsh"
+_safe_source "$ZSH_CONFIG_DIR/alias/alias_editor.zsh"
+_safe_source "$ZSH_CONFIG_DIR/alias/alias_eza.zsh"
+_safe_source "$ZSH_CONFIG_DIR/alias/alias_cd.zsh"
 
-_safe_source "$ZSH_CONFIG_DIR/alias_git.zsh"
-_safe_source "$ZSH_CONFIG_DIR/alias_tmux.zsh"
+_safe_source "$ZSH_CONFIG_DIR/alias/alias_git.zsh"
+_safe_source "$ZSH_CONFIG_DIR/alias/alias_tmux.zsh"
 
 alias valias="source $ZSH_CONFIG_DIR/alias.zsh"
 
