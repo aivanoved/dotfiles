8e0caa605d279b372fc8f6d3fd28aeb5cb926c18
Alexander Ivanov <alexander.valentinov.ivanov@gmail.com>
2025-03-01 14:29:35 +0200

    reorde nvim maps

--- nvim/.config/nvim/lua/aivanoved/remap.lua
+++ nvim/.config/nvim/lua/aivanoved/remap.lua
@@ -1,55 +1,58 @@
+-- hjkl maps
+
 vim.keymap.set('i', 'jj', '<Esc>')
-
-vim.keymap.set({ 'n', 'x' }, 'm', 'd')
-vim.keymap.set('n', 'mm', 'dd')
-
-vim.keymap.set('n', 'M', 'D')
-
--- stylua: ignore start
-vim.keymap.set({ 'n', 'x' }, 'd', '\"_d')
-vim.keymap.set({ 'n', 'x' }, 'dd', '\"_dd')
-vim.keymap.set({ 'n', 'x' }, 'D', '\"_D')
-
-vim.keymap.set({ 'n', 'x' }, 'c', '\"_c')
-vim.keymap.set({ 'n', 'x' }, 'cc', '\"_cc')
-vim.keymap.set({ 'n', 'x' }, 'C', '\"_C')
-
-vim.keymap.set({ 'n', 'x' }, 's', '\"_s')
-vim.keymap.set({ 'n', 'x' }, 'ss', '\"_ss')
-vim.keymap.set({ 'n', 'x' }, 'S', '\"_S')
--- stylua: ignore end
-
+vim.keymap.set('n', 'J', 'mzJ`z`')
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+
 vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
 
-vim.keymap.set('n', 'Y', 'yg$')
-vim.keymap.set('n', 'J', 'mzJ`z`')
+vim.keymap.set('n', '<leader>k', '<cmd>lnext<CR>zz')
+vim.keymap.set('n', '<leader>j', '<cmd>lprev<CR>zz')
+
+-- scroll maps
 vim.keymap.set('n', '<c-d>', '<c-d>zz')
 vim.keymap.set('n', '<c-u>', '<c-u>zz')
+
+-- search maps
 vim.keymap.set('n', 'n', 'nzzzv')
 vim.keymap.set('n', 'N', 'Nzzzv')
--- stylua: ignore start
-vim.keymap.set('x', '<leader>p', '\"_dP')
 
-vim.keymap.set('n', '<leader>y', '\"+y')
-vim.keymap.set('v', '<leader>y', '\"+y')
-vim.keymap.set('n', '<leader>Y', '\"+Y')
+-- yank maps
+vim.keymap.set('n', 'Y', 'yg$')
 
-vim.keymap.set('n', '<leader>p', '\"+p')
-vim.keymap.set('n', '<leader>P', '\"+P')
-vim.keymap.set('x', '<leader>P', '\"_d\"+p')
--- stylua: ignore end
+vim.keymap.set({ 'n', 'v' }, '<leader>y', '"+y')
+vim.keymap.set('n', '<leader>Y', '"+Y')
 
+-- delete maps
+vim.keymap.set({ 'n', 'x' }, 'd', '"_d')
+vim.keymap.set({ 'n', 'x' }, 'dd', '"_dd')
+vim.keymap.set({ 'n', 'x' }, 'D', '"_D')
+
+-- change maps
+vim.keymap.set({ 'n', 'x' }, 'c', '"_c')
+vim.keymap.set({ 'n', 'x' }, 'cc', '"_cc')
+vim.keymap.set({ 'n', 'x' }, 'C', '"_C')
+
+-- substitute maps
+vim.keymap.set({ 'n', 'x' }, 's', '"_s')
+vim.keymap.set({ 'n', 'x' }, 'ss', '"_ss')
+vim.keymap.set({ 'n', 'x' }, 'S', '"_S')
+
+-- substitution maps
+vim.keymap.set('n', '<leader>s', ':%s/\\<<c-r><c-w>\\>/<c-r><c-w>/gI<Left><Left><Left>')
+
+-- paste maps
+vim.keymap.set('n', '<leader>p', '"+p')
+vim.keymap.set('n', '<leader>P', '"+P')
+vim.keymap.set('x', '<leader>p', '"_dP')
+vim.keymap.set('x', '<leader>P', '"_d"+p')
+
+-- ex mode maps
 vim.keymap.set('n', 'Q', '<nop>')
 
 -- vim.keymap.set('n', '<c-k>', '<cmd>cnext<CR>zz')
 -- vim.keymap.set('n', '<c-j>', '<cmd>cprev<CR>zz')
 
-vim.keymap.set('n', '<leader>k', '<cmd>lnext<CR>zz')
-
-vim.keymap.set('n', '<leader>j', '<cmd>lprev<CR>zz')
-
-vim.keymap.set('n', '<leader>s', ':%s/\\<<c-r><c-w>\\>/<c-r><c-w>/gI<Left><Left><Left>')
 vim.keymap.set('n', '<leader>x', '<cmd>!chmod +x %<CR>', { silent = true })
 
 vim.keymap.set('n', '<leader>bf', function()
