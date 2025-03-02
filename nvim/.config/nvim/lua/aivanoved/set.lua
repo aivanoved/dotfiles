7a77d0870b665eb2ae97574b8416d5c929d45742
Alexander Ivanov <alexander.valentinov.ivanov@gmail.com>
2025-03-02 16:29:19 +0200

    remove auto indent

--- nvim/.config/nvim/lua/aivanoved/set.lua
+++ nvim/.config/nvim/lua/aivanoved/set.lua
@@ -12,7 +12,6 @@ vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
 vim.opt.expandtab = true
 vim.opt.smartindent = true
-vim.opt.autoindent = true
 vim.opt.wrap = false
 
 -- vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
