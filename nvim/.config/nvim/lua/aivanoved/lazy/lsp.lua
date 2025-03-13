d5029bd36414570712a481f2b53d46a9876f172a
Alexander Ivanov <alexander.valentinov.ivanov@gmail.com>
2025-03-13 12:26:38 +0200

    misc(nvim): reorder lsp config functions

--- nvim/.config/nvim/lua/aivanoved/lazy/lsp.lua
+++ nvim/.config/nvim/lua/aivanoved/lazy/lsp.lua
@@ -1,69 +1,4 @@
 ---@param servers string[]
-local function mason_config(servers)
-    local lsp_zero = require('lsp-zero')
-
-    local lspconfig = require('lspconfig')
-    local mason = require('mason')
-    local mason_lspconfig = require('mason-lspconfig')
-
-    mason.setup({})
-    mason_lspconfig.setup({
-        ensure_installed = servers,
-        handlers = {
-            lsp_zero.default_setup,
-            lua_ls = function()
-                local lua_opts = lsp_zero.nvim_lua_ls()
-                lspconfig.lua_ls.setup(lua_opts)
-            end,
-            rust_analyzer = lsp_zero.noop,
-        },
-    })
-end
-
-local function lsp_keymaps()
-    local lsp_zero = require('lsp-zero')
-
-    lsp_zero.on_attach(function(client, bufnr)
-        local opts = { buffer = bufnr, remap = false }
-
-        vim.keymap.set('n', 'gd', function()
-            vim.lsp.buf.definition()
-        end, opts)
-        vim.keymap.set('n', 'K', function()
-            vim.lsp.buf.hover()
-        end, opts)
-        vim.keymap.set('n', '<leader>vws', function()
-            vim.lsp.buf.workspace_symbol()
-        end, opts)
-        vim.keymap.set('n', '<leader>vd', function()
-            vim.diagnostic.open_float()
-        end, opts)
-        vim.keymap.set('n', '[d', function()
-            vim.diagnostic.goto_next()
-        end, opts)
-        vim.keymap.set('n', ']d', function()
-            vim.diagnostic.goto_prev()
-        end, opts)
-        vim.keymap.set('n', '<leader>vca', function()
-            vim.lsp.buf.code_action()
-        end, opts)
-        vim.keymap.set('n', '<leader>vrr', function()
-            vim.lsp.buf.references()
-        end, opts)
-        vim.keymap.set('n', '<leader>vrn', function()
-            vim.lsp.buf.rename()
-        end, opts)
-        vim.keymap.set('i', '<C-h>', function()
-            vim.lsp.buf.signature_help()
-        end, opts)
-
-        -- if client.server_capabilities.inlayHintProvider then
-        --     vim.lsp.inlay_hint.enable(bufnr, true)
-        -- end
-    end)
-end
-
----@params servers string[]
 local function lspconfigure(servers)
     local cmp_capabilities = require('cmp_nvim_lsp').default_capabilities()
 
@@ -102,6 +37,28 @@ local function lspconfigure(servers)
     for _, lsp in ipairs(servers) do
         lspconfig[lsp].setup(server_setup[lsp])
     end
+end
+
+---@param servers string[]
+local function mason_config(servers)
+    local lsp_zero = require('lsp-zero')
+
+    local lspconfig = require('lspconfig')
+    local mason = require('mason')
+    local mason_lspconfig = require('mason-lspconfig')
+
+    mason.setup({})
+    mason_lspconfig.setup({
+        ensure_installed = servers,
+        handlers = {
+            lsp_zero.default_setup,
+            lua_ls = function()
+                local lua_opts = lsp_zero.nvim_lua_ls()
+                lspconfig.lua_ls.setup(lua_opts)
+            end,
+            rust_analyzer = lsp_zero.noop,
+        },
+    })
 end
 
 local function cmp_setup()
@@ -154,6 +111,49 @@ local function lsp_diagnostics()
     vim.diagnostic.config({
         virtual_text = true,
     })
+end
+
+local function lsp_keymaps()
+    local lsp_zero = require('lsp-zero')
+
+    lsp_zero.on_attach(function(client, bufnr)
+        local opts = { buffer = bufnr, remap = false }
+
+        vim.keymap.set('n', 'gd', function()
+            vim.lsp.buf.definition()
+        end, opts)
+        vim.keymap.set('n', 'K', function()
+            vim.lsp.buf.hover()
+        end, opts)
+        vim.keymap.set('n', '<leader>vws', function()
+            vim.lsp.buf.workspace_symbol()
+        end, opts)
+        vim.keymap.set('n', '<leader>vd', function()
+            vim.diagnostic.open_float()
+        end, opts)
+        vim.keymap.set('n', '[d', function()
+            vim.diagnostic.goto_next()
+        end, opts)
+        vim.keymap.set('n', ']d', function()
+            vim.diagnostic.goto_prev()
+        end, opts)
+        vim.keymap.set('n', '<leader>vca', function()
+            vim.lsp.buf.code_action()
+        end, opts)
+        vim.keymap.set('n', '<leader>vrr', function()
+            vim.lsp.buf.references()
+        end, opts)
+        vim.keymap.set('n', '<leader>vrn', function()
+            vim.lsp.buf.rename()
+        end, opts)
+        vim.keymap.set('i', '<C-h>', function()
+            vim.lsp.buf.signature_help()
+        end, opts)
+
+        -- if client.server_capabilities.inlayHintProvider then
+        --     vim.lsp.inlay_hint.enable(bufnr, true)
+        -- end
+    end)
 end
 
 local function lsp_config()
