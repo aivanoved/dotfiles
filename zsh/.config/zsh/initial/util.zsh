3e59b7095fb1836be4073aba2ae01ee8bc61d9ed
Alexander Ivanov <alexander.valentinov.ivanov@gmail.com>
2025-04-17 13:30:56 +0300

    feat(zsh): move util folder into directory have initial sourcing

---
+++ zsh/.config/zsh/initial/util.zsh
@@ -0,0 +1,86 @@
+if [[ ${_UTIL_FILE_SOURCE:-0} -eq 1 ]]; then
+    return 0
+fi
+
+export _UTIL_FILE_SOURCE=1
+
+local _DEBUG=0
+local _INFO=1
+local _WARNING=2
+local _ERROR=3
+
+local _DEFAULT_LOG_VERBOCITY=$_ERROR
+export _UTIL_LOG_VERBOCITY="${_UTIL_LOG_VERBOCITY:-$_DEFAULT_LOG_VERBOCITY}"
+
+_log_format() {
+    local log_level=$1
+    local message=$2
+
+    local log_prefix=""
+    case $log_level in
+        $_DEBUG) log_prefix="DEBUG: " ;;
+        $_INFO) log_prefix="INFO: " ;;
+        $_WARNING) log_prefix="WARNING: " ;;
+        $_ERROR) log_prefix="ERROR: " ;;
+        *) log_prefix="UNKNOWN: " ;;
+    esac
+
+    case $log_level in
+        $_DEBUG) log_prefix="\e[34m$log_prefix\e[0m" ;; # Blue
+        $_INFO) log_prefix="\e[32m$log_prefix\e[0m" ;;  # Green
+        $_WARNING) log_prefix="\e[33m$log_prefix\e[0m" ;; # Yellow
+        $_ERROR) log_prefix="\e[31m$log_prefix\e[0m" ;;  # Red
+        *) log_prefix="\e[37m$log_prefix\e[0m" ;; # White
+    esac
+
+    local formatted_message="${log_prefix}${message}"
+
+    echo "$formatted_message"
+}
+
+_util_log() {
+    local log_level="${_UTIL_LOG_VERBOCITY:-$_DEFAULT_LOG_VERBOCITY}"
+    local level=$1
+    local message=$2
+
+    if [ $level -gt $log_level ]; then
+        _log_format $level "$message"
+    fi
+
+}
+
+
+_set_log_level() {
+    local log_level=$1
+    if [[ $log_level =~ ^[0-3]$ ]]; then
+        _UTIL_LOG_VERBOCITY=$log_level
+        _util_log 0 "Log level set to $log_level"
+    else
+        _util_log 2 "Invalid log level: $log_level. Must be between 0 and 3."
+    fi
+}
+
+_temporary_log_level() {
+    local temp_level=$1
+    local original_level=$_UTIL_LOG_VERBOCITY
+    _set_log_level $temp_level
+    export _util_original_log_level=$original_level
+}
+
+_restore_log_level() {
+    if [[ -n $_util_original_log_level ]]; then
+        _UTIL_LOG_VERBOCITY=$_util_original_log_level
+        unset _util_original_log_level
+    else
+        _util_log 2 "No temporary log level set."
+    fi
+}
+
+_safe_source(){
+    if [ -f $1 ]; then
+        _util_log 1 "Sourcing $1"
+        source $1
+    else
+        _util_log 2 "File $1 not found, not sourcing"
+    fi
+}
