816675cb694bd43449e7e07d9f21a9317a01baa7
Alexander Ivanov <alexander.valentinov.ivanov@gmail.com>
2025-05-02 11:14:12 +0300

    feat(nvim): chatgpt

---
+++ nvim/.config/nvim/lua/aivanoved/lazy/conform/formatter.lua
@@ -0,0 +1,93 @@
+--- @alias aivanoved.conform.Filetype string
+
+--- @alias aivanoved.conform.Path string
+
+--- @class aivanoved.conform.FormatterParams
+--- @field filetype aivanoved.conform.Filetype
+--- @field args string[]
+
+--- @class aivanoved.conform.Formatter
+--- @field name string
+--- @field filetypes aivanoved.conform.Filetype[]
+--- @field default_executable string|nil
+--- @field params aivanoved.conform.FormatterParams[]
+--- @field paths aivanoved.conform.Path[]
+Formatter = {}
+
+--- @param name string
+--- @param filetypes string|string[]
+--- @param default_executable string|nil
+--- @param params table<aivanoved.conform.Filetype, aivanoved.conform.FormatterParams[]>|nil
+--- @param paths string[]|nil
+--- @return aivanoved.conform.Formatter
+function Formatter:new(name, filetypes, default_executable, params, paths)
+    if type(filetypes) == 'string' then
+        filetypes = { filetypes }
+    end
+
+    local obj = {
+        name = name,
+        filetypes = filetypes,
+        default_executable = default_executable,
+        params = params or {},
+        paths = paths or {},
+    }
+
+    setmetatable(obj, self)
+    self.__index = self
+
+    return obj
+end
+
+--- @return conform.FormatterConfigOverride|nil
+function Formatter:into_conform_config()
+    if not self.default_executable then
+        return nil
+    end
+    -- check if python formatter
+    local is_python = false
+    for _, ft in ipairs(self.filetypes) do
+        if ft == 'python' then
+            is_python = true
+            break
+        end
+    end
+
+    if not is_python then
+        return nil
+    end
+
+    -- check if in pixi environment
+    if vim.fn.executable('pixi') == 0 then
+        return nil
+    end
+    -- check if pixi directory exists anywhare upwards
+    local pixi_dir = vim.fn.finddir('.pixi', vim.fn.getcwd() .. ';')
+
+    vim.notify(
+        'Pixi directory: ' .. pixi_dir,
+        vim.log.levels.INFO,
+        { title = 'Pixi Directory' }
+    )
+    if pixi_dir == '' then
+        return nil
+    end
+
+    local command =
+        require('conform.util').find_executable(self.paths, self.default_executable)
+
+    --- @type conform.FormatterConfigOverride
+    local config = {
+        -- check if the executable is in the pixi environment
+        command = require('conform.util').find_executable(
+            self.paths,
+            self.default_executable
+        ),
+    }
+
+    return config
+end
+
+return {
+    Formatter = Formatter,
+}
--- nvim/.config/nvim/lua/aivanoved/lazy/conform/init.lua
+++ nvim/.config/nvim/lua/aivanoved/lazy/conform/init.lua
@@ -1,76 +1,73 @@
---- @alias Filetype string
+local Formatter = require('aivanoved.lazy.conform.formatter').Formatter
 
---- @alias Path string
-
---- @class FormatterParams
---- @field filetype Filetype
---- @field args string[]
-
---- @class Formatter
---- @field name string
---- @field filetypes Filetype[]
---- @field params? FormatterParams[]
---- @field default_executable? string
---- @field default_path? Path
-
---- @type Formatter
-local stylua = {
-    name = 'stylua',
-    filetypes = { 'lua' },
-    default_executable = 'stylua',
-}
-
---- @type Formatter
-local rustfmt = {
-    name = 'rustfmt',
-    filetypes = { 'rust' },
-    default_executable = 'rustfmt',
-}
-
---- @type Formatter
-local prettier = {
-    name = 'prettier',
-    filetypes = { 'javascript' },
-    default_executable = 'prettier',
-}
-
---- @type Formatter
-local ruff = {
-    name = 'ruff_format',
-    filetypes = { 'python' },
-    default_executable = 'ruff',
-}
-
---- @type Formatter
-local isort = {
-    name = 'isort',
-    filetypes = { 'python' },
-    default_executable = 'isort',
-}
-
---- @type Formatter
-local black = {
-    name = 'black',
-    filetypes = { 'python' },
-    default_executable = 'black',
-}
+local stylua = Formatter:new('stylua', 'lua', nil, nil, nil)
+local rustfmt = Formatter:new('rustfmt', 'rust', nil, nil, nil)
+local prettier = Formatter:new('prettier', 'javascript', nil, nil, nil)
+local ruff_format = Formatter:new('ruff_format', 'python', nil, nil, nil)
+local isort = Formatter:new('isort', 'python', 'isort', nil, { '.pixi' })
+local black = Formatter:new('black', 'python', nil, nil, nil)
 
 local all_formatters = {
     stylua,
     rustfmt,
     prettier,
-    ruff,
+    -- ruff_format,
     isort,
     black,
+}
+
+--- @type table<string, conform.FiletypeFormatterInternal>
+FILETYPE_INTERNALS = {}
+
+FILETYPE_INTERNALS['lua'] = {
+    lsp_format = 'fallback',
+}
+
+FILETYPE_INTERNALS['python'] = {
+    lsp_format = 'fallback',
 }
 
 local function config()
     local conform = require('conform')
 
+    --- @type table<string, conform.FiletypeFormatterInternal>
+    local formatters_by_ft = {}
+
+    for ft, internal in pairs(FILETYPE_INTERNALS) do
+        formatters_by_ft[ft] = internal
+    end
+
+    for _, formatter in ipairs(all_formatters) do
+        for _, ft in ipairs(formatter.filetypes) do
+            if formatters_by_ft[ft] then
+                local internal = formatters_by_ft[ft]
+                table.insert(internal, formatter.name)
+            end
+        end
+    end
+
+    --- @type table<string, conform.FormatterConfigOverride>
+    local formatters = {}
+
+    for _, formatter in ipairs(all_formatters) do
+        local to_insert = false
+
+        for _, ft in ipairs(formatter.filetypes) do
+            if formatters_by_ft[ft] then
+                to_insert = true
+                break
+            end
+        end
+
+        local formatter_config = formatter:into_conform_config()
+
+        if to_insert and formatter_config then
+            formatters[formatter.name] = formatter_config
+        end
+    end
+
     conform.setup({
-        formatters_by_ft = {
-            lua = { stylua.name, lsp_format = 'fallback' },
-        },
+        formatters_by_ft = formatters_by_ft,
         format_on_save = function(bufnr)
             if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
                 return
@@ -80,6 +77,7 @@ local function config()
                 lsp_format = 'fallback',
             }
         end,
+        formatters = formatters,
     })
 
     vim.api.nvim_create_user_command('FormatDisable', function(args)
