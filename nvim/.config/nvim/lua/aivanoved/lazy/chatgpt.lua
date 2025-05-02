75b6f75b1846438f3193c24b8b517d261ff87055
Alexander Ivanov <alexander.valentinov.ivanov@gmail.com>
2025-05-02 11:34:17 +0300

    feat(nvim): use neawer gpt

--- nvim/.config/nvim/lua/aivanoved/lazy/chatgpt.lua
+++ nvim/.config/nvim/lua/aivanoved/lazy/chatgpt.lua
@@ -2,7 +2,9 @@ return {
     'jackMort/ChatGPT.nvim',
     event = 'VeryLazy',
     config = function()
-        require('chatgpt').setup()
+        require('chatgpt').setup({
+            model = 'gpt-4.1',
+        })
     end,
     dependencies = {
         'MunifTanjim/nui.nvim',
