842b73a9d23407168a03dd2e7b0a983856ac1f10
Alexander Ivanov <alexander.valentinov.ivanov@gmail.com>
2025-07-30 15:43:46 +0300

    feat(nvim): diagnostics

--- nvim/.config/nvim/lua/aivanoved/lazy/lsp/diagnostics.lua
+++ nvim/.config/nvim/lua/aivanoved/lazy/lsp/diagnostics.lua
@@ -1,15 +1,35 @@
-local function lsp_diagnostics()
-    local lsp_zero = require('lsp-zero')
-    lsp_zero.set_sign_icons({
-        error = ' ',
-        warning = ' ',
-        info = '󰋽 ',
-        hint = '󰌶 ',
-    })
+--- @type table<vim.diagnostic.Severity, string>
+local DIAGNOSTIC_SIGNS = {
+    [vim.diagnostic.severity.ERROR] = ' ',
+    [vim.diagnostic.severity.WARN] = ' ',
+    [vim.diagnostic.severity.INFO] = '󰋽 ',
+    [vim.diagnostic.severity.HINT] = '󰌶 ',
+}
 
-    vim.diagnostic.config({
-        virtual_text = true,
-    })
+local NUMBER_LINE_HIGHLIGHT = {
+    [vim.diagnostic.severity.ERROR] = 'DiagnosticError',
+    [vim.diagnostic.severity.WARN] = 'DiagnosticWarn',
+    [vim.diagnostic.severity.INFO] = 'DiagnosticInfo',
+    [vim.diagnostic.severity.HINT] = 'DiagnosticHint',
+}
+
+local function lsp_diagnostics()
+    --- @type vim.diagnostic.Opts
+    local diagnostic_opt = {
+        underline = true,
+        update_in_insert = true,
+        virtual_text = {
+            source = true,
+            current_line = true,
+            virt_text_pos = 'eol_right_align',
+        },
+        signs = {
+            text = DIAGNOSTIC_SIGNS,
+            numhl = NUMBER_LINE_HIGHLIGHT,
+        }
+    }
+
+    vim.diagnostic.config(diagnostic_opt)
 end
 
 local M = {}
