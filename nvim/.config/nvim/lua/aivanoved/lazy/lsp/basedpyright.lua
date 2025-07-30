57143d25f2f24ec7d5305d2395039b14f68f0856
Alexander Ivanov <alexander.valentinov.ivanov@gmail.com>
2025-07-30 12:42:40 +0300

    feat(nvim): add default commands

--- nvim/.config/nvim/lua/aivanoved/lazy/lsp/basedpyright.lua
+++ nvim/.config/nvim/lua/aivanoved/lazy/lsp/basedpyright.lua
@@ -1,6 +1,10 @@
 local typedef = require('aivanoved.lazy.lsp.typedef')
+local default_config = require('lspconfig.configs.basedpyright')
 
 return typedef.validate_lsp_config({
     lsp_name = 'basedpyright',
     ensure_installed = true,
+    config = {
+        commands = default_config.commands
+    },
 })
