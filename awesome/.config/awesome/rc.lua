aadebeedf0d68bd780e33b66d200338e1504dcf1
Alexander Ivanov <54221777+aivanoved@users.noreply.github.com>
2025-12-21 17:46:05 +0200

    feat(zsh): add cow say

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -62,7 +62,7 @@ beautiful.init(
 )
 
 -- This is used later as the default terminal and editor to run.
-terminal = 'ghostty'
+terminal = 'alacritty'
 editor = os.getenv('EDITOR') or 'nvim'
 editor_cmd = terminal .. ' -e ' .. editor
 
